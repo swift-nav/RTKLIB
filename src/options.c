swift-nav/RTKLIB

commit 1c45a3c51365d2b1acd203af0ce87138211a73da

Additional command line options for rnx2rtkp (#58)
* Add -z flag which enables output of SPP solution when DGPS/float/fix/PPP position not available
* Add -rb flag to allow configuration of base position mode
* Add new position mode (POSOPT_RINEX_DYN == 6) which reads base station positions from RINEX header and new site occupancy events

src/options.c
@@ -56,7 +56,7 @@ static char snrmask_[NFREQ][1024];
 #define STAOPT  "0:all,1:single"
 #define STSOPT  "0:off,1:state,2:residual"
 #define ARMOPT  "0:off,1:continuous,2:instantaneous,3:fix-and-hold,4:WLNL,5:TCAR,6:wide-lane"
-#define POSOPT  "0:llh,1:xyz,2:single,3:posfile,4:rinexhead,5:rtcm,6:raw"
+#define POSOPT  "0:llhxyz,1:single,2:posfile,3:rinexhead,4:rtcm,5:raw,6:rinexdynamic"
 #define TIDEOPT "0:off,1:on,2:otl"
 #define PHWOPT  "0:off,1:on,2:precise"
 

app/rnx2rtkp/rnx2rtkp.c
@@ -28,7 +28,7 @@ static const char *help[]={
 "",
 " usage: rnx2rtkp [option]... file file [...]",
 "",
-" Read RINEX OBS/NAV/GNAV/HNAV/CLK, SP3, SBAS message log files and ccompute ",
+" Read RINEX OBS/NAV/GNAV/HNAV/CLK, SP3, SBAS message log files and compute ",
 " receiver (rover) positions and output position solutions.",
 " The first RINEX OBS file shall contain receiver (rover) observations. For the",
 " relative mode, the second RINEX OBS file shall contain reference",
@@ -64,10 +64,15 @@ static const char *help[]={
 " -g        output latitude/longitude in the form of ddd mm ss.ss' [ddd.ddd]",
 " -t        output time in the form of yyyy/mm/dd hh:mm:ss.ss [sssss.ss]",
 " -u        output time in utc [gpst]",
+" -z        output single point position when unable to compute",
+"           DGPS/float/fix/PPP position",
 " -d col    number of decimals in time [3]",
 " -s sep    field separator [' ']",
 " -r x y z  reference (base) receiver ecef pos (m) [average of single pos]",
 "           rover receiver ecef pos (m) for fixed or ppp-fixed mode",
+" -rb mode  reference (base) receiver position mode (1:average of single pos,",
+"           2:read from file,3:read from RINEX header,6:read from RINEX header",
+"           and subsequent site occupancy events) [1]",
 " -l lat lon hgt reference (base) receiver latitude/longitude/height (deg/m)",
 "           rover latitude/longitude/height for fixed or ppp-fixed mode",
 " -y level  output soltion status (0:off,1:states,2:residuals) [0]",
@@ -155,27 +160,27 @@ int main(int argc, char **argv)
         else if (!strcmp(argv[i],"-d")&&i+1<argc) solopt.timeu=atoi(argv[++i]);
         else if (!strcmp(argv[i],"-b")) prcopt.soltype=1;
         else if (!strcmp(argv[i],"-c")) prcopt.soltype=2;
-        else if (!strcmp(argv[i],"-i")) prcopt.modear=2;
+        else if (!strcmp(argv[i],"-i")) prcopt.modear=ARMODE_INST;
         else if (!strcmp(argv[i],"-h")) {
-            if (prcopt.modear!=ARMODE_WL) prcopt.modear=3;
+            if (prcopt.modear!=ARMODE_WL) prcopt.modear=ARMODE_FIXHOLD;
             prcopt.wlmodear=1;
         }
         else if (!strcmp(argv[i],"-t")) solopt.timef=1;
         else if (!strcmp(argv[i],"-u")) solopt.times=TIMES_UTC;
+        else if (!strcmp(argv[i],"-z")) prcopt.outsingle=1;
         else if (!strcmp(argv[i],"-e")) solopt.posf=SOLF_XYZ;
         else if (!strcmp(argv[i],"-a")) solopt.posf=SOLF_ENU;
         else if (!strcmp(argv[i],"-n")) solopt.posf=SOLF_NMEA;
         else if (!strcmp(argv[i],"-g")) solopt.degf=1;
-	    else if (!strcmp(argv[i],"-w")) { /* enable widelane mode */
-            prcopt.modear=ARMODE_WL;
-        }
+        else if (!strcmp(argv[i],"-w")) prcopt.modear=ARMODE_WL;
         else if (!strcmp(argv[i],"-r")&&i+3<argc) {
-            prcopt.refpos=prcopt.rovpos=0;
+            prcopt.refpos=prcopt.rovpos=POSOPT_POS;
             for (j=0;j<3;j++) prcopt.rb[j]=atof(argv[++i]);
             matcpy(prcopt.ru,prcopt.rb,3,1);
         }
+        else if (!strcmp(argv[i],"-rb")&&i+1<argc) prcopt.refpos=atoi(argv[++i]);
         else if (!strcmp(argv[i],"-l")&&i+3<argc) {
-            prcopt.refpos=prcopt.rovpos=0;
+            prcopt.refpos=prcopt.rovpos=POSOPT_POS;
             for (j=0;j<3;j++) pos[j]=atof(argv[++i]);
             for (j=0;j<2;j++) pos[j]*=D2R;
             pos2ecef(pos,prcopt.rb);

src/rinex.c
@@ -1005,7 +1005,14 @@ static int readrnxobsb(FILE *fp, const char *opt, double ver, int *tsys,
             data[n].sat=(unsigned char)sats[i-1];
             
             /* decode obs data */
-            if (decode_obsdata(fp,buff,ver,mask,index,data+n)&&n<MAXOBS) n++;
+            if (decode_obsdata(fp,buff,ver,mask,index,data+n)&&n<MAXOBS) {
+                if (sta) {
+                    data[n].refpos[0]=sta->pos[0];
+                    data[n].refpos[1]=sta->pos[1];
+                    data[n].refpos[2]=sta->pos[2];
+                }
+                n++;
+            }
         }
         else if (*flag==3||*flag==4) { /* new site or header info follows */
             

src/rtklib.h
@@ -412,6 +412,7 @@ extern "C" {
 #define POSOPT_RINEX 3                  /* pos option: rinex header pos */
 #define POSOPT_RTCM  4                  /* pos option: rtcm station pos */
 #define POSOPT_RAW   5                  /* pos option: raw station pos */
+#define POSOPT_RINEX_DYN 6              /* pos option: rinex site occupation pos */
 
 #define STR_NONE     0                  /* stream type: none */
 #define STR_SERIAL   1                  /* stream type: serial */
@@ -541,6 +542,8 @@ typedef struct {        /* observation data record */
     double L[NFREQ+NEXOBS]; /* observation data carrier-phase (cycle) */
     double P[NFREQ+NEXOBS]; /* observation data pseudorange (m) */
     float  D[NFREQ+NEXOBS]; /* observation data doppler frequency (Hz) */
+    /* only used if refpos == POSOPT_RINEX_DYN */
+    double refpos[3];   /* station coordinates associated with this observation */
 } obsd_t;
 
 typedef struct {        /* observation data */

src/rtkpos.c
@@ -2120,6 +2120,13 @@ extern int rtkpos(rtk_t *rtk, const obsd_t *obs, int n, const nav_t *nav)
     for (nu=0;nu   <n&&obs[nu   ].rcv==1;nu++) ;
     for (nr=0;nu+nr<n&&obs[nu+nr].rcv==2;nr++) ;
 
+    if (opt->refpos==POSOPT_RINEX_DYN&&opt->mode!=PMODE_SINGLE&&
+        opt->mode!=PMODE_MOVEB) {
+        if (nr > 0) {
+            for (i=0;i<6;i++) rtk->rb[i]=i<3?obs[nu].refpos[i]:0.0;
+        }
+    }
+
     time=rtk->sol.time; /* previous epoch */
 
     /* rover position by single point positioning */

commit 37909323fe9d5b6817fb97af3deeda23e26c245e

Fix bug introduced in 1c45a3c51365d2b1acd203af0ce87138211a73da: buff2sysopts() has special handling for the 0 and 1 values

src/options.c
@@ -56,7 +56,7 @@ static char snrmask_[NFREQ][1024];
 #define STAOPT  "0:all,1:single"
 #define STSOPT  "0:off,1:state,2:residual"
 #define ARMOPT  "0:off,1:continuous,2:instantaneous,3:fix-and-hold,4:WLNL,5:TCAR,6:wide-lane"
-#define POSOPT  "0:llhxyz,1:single,2:posfile,3:rinexhead,4:rtcm,5:raw,6:rinexdynamic"
+#define POSOPT  "0:llh,1:xyz,2:single,3:posfile,4:rinexhead,5:rtcm,6:raw,7:rinexdynamic"
 #define TIDEOPT "0:off,1:on,2:otl"
 #define PHWOPT  "0:off,1:on,2:precise"
 